b59e50238e97dea5d8744719fade22f45c614c40
YUJIN511 <43337244+YUJIN511@users.noreply.github.com>
2019-11-21 00:45:02 +0900

    Create Reverse_Integer.cpp

---
+++ Easy/Reverse_Integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+        long long result = 0;
+        
+        while (x!= 0) {
+			    result = result * 10 + x % 10;
+			    x /= 10;
+          if (result < pow(2, 31) * -1 || result > pow(2, 31) - 1) {
+              return 0;
+          }
+		    }
+            
+        return result;
+
+    }
+};
